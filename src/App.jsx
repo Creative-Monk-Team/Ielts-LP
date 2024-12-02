e6607700268615369cd0b0e62012f7818892c323
UttamCreativeMonk <uttam.creativemonk@gmail.com>
2024-12-02 17:56:37 +0530

    solved 1 isue

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { ToastContainer, toast, Bounce } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import AllRoutes from "./routes/Allroutes";
+import AllRoutes from "./routes/AllRoutes";
 import { HelmetProvider } from "react-helmet-async";
 import PopupForm from "./components/PopupForm";
 import WaitingLoader from "./components/WaitingLoader";
