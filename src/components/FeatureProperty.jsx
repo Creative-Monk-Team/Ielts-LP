64151535998129d77d25d1ff1d24a9a76c53bec4
Creative-Monk-Team <team.creativemonk@gmail.com>
2024-12-12 13:27:39 +0530

    added speaker details

--- src/components/FeatureProperty.jsx
+++ src/components/FeatureProperty.jsx
@@ -4,6 +4,7 @@ import BannerImage3 from "../assets/banner03_Recent.jpg";
 import Button from "./Button";
 import p2 from "../assets/p2.png";
 import speaker from "../assets/speaker3.png";
+import britishCouncil from "../assets/logo-br-co.png";
 
 let FeatureProperty = () => {
   let aboutRef = useRef(null);
@@ -48,13 +49,13 @@ let FeatureProperty = () => {
           </div>
           <h1>Meet the Webinar Leader</h1>
           <h4>
-            Our webinar is led by [Instructor’s Name], a seasoned IELTS trainer
+            Our webinar is led by Ms. Sanjita, a seasoned IELTS trainer
             with over [X] years of experience.
           </h4>
           <ul>
             <li>
               <p>
-                [Instructor’s Name] has successfully helped over 1,000+ students
+                Ms. Sanjita has successfully helped over 1,000+ students
                 clear their IELTS exams and achieve their study abroad dreams.
               </p>
             </li>
@@ -78,6 +79,10 @@ let FeatureProperty = () => {
           }`}
         >
           <img alt="First Image" className={styles.bigImage} src={speaker} />
+          <div className={styles.overlayDiv}>
+            <h4>Certified by</h4>
+            <img className={styles.brLogo} src={britishCouncil}/>
+          </div>
         </div>
       </div>
     </div>
